4b928ac7cc5cc2555263703bc163776040f1e795
lautaro3870 <lautarodisbro@gmail.com>
2025-02-13 19:56:41 +0100

    feat: render list of exercices

--- src/app/components/ExerciseTable.tsx
+++ src/app/components/ExerciseTable.tsx
@@ -1,67 +1,180 @@
-import { useMemo } from "react";
-import {
-  MaterialReactTable,
-  useMaterialReactTable,
-  type MRT_ColumnDef,
-} from "material-react-table";
+import { useState } from "react";
+
+import { Button, Input } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 type Exercise = {
   exercise: {
+    id: number;
     name: string;
     weigth: string;
-    sets: string[]
+    inputs: JSX.Element[];
+    sets: [
+      {
+        id: number;
+        repetitions: string;
+      }
+    ];
   };
 };
 
-//nested data is ok, see accessorKeys in ColumnDef below
 const data: Exercise[] = [
   {
     exercise: {
+      id: 1,
       name: "Pecho sentado",
       weigth: "70",
-      sets: ['9-8-9', '10-9-8']
+      inputs: [],
+      sets: [
+        {
+          id: 1,
+          repetitions: "10, 8, 9",
+        },
+      ],
+    },
+  },
+  {
+    exercise: {
+      id: 2,
+      name: "Sentadill",
+      weigth: "70",
+      inputs: [],
+      sets: [
+        {
+          id: 1,
+          repetitions: "10, 8, 9",
+        },
+      ],
+    },
+  },
+  {
+    exercise: {
+      id: 3,
+      name: "Pecho sentado",
+      weigth: "70",
+      inputs: [],
+      sets: [
+        {
+          id: 1,
+          repetitions: "10, 8, 9",
+        },
+      ],
     },
   },
 ];
 
 export default function ExerciseTable() {
-  const columns = useMemo<MRT_ColumnDef<Exercise>[]>(
-    () => [
-      {
-        accessorKey: "exercise.name", //access nested data with dot notation
-        header: "Ejecicio",
-        size: 120,
-      },
-      {
-        accessorKey: "exercise.weigth", //access nested data with dot notation
-        header: "Peso",
-        size: 90,
-      },
-      {
-        accessorKey: "exercise.sets", //access nested data with dot notation
-        header: "Sets",
-        size: 190,
-      },
-    ],
-    []
-  );
+//   const renderInputs = (inputs: any) => {
+//     return (
+//       <div
+//         style={{
+//           width: 200,
+//           overflowX: "scroll",
+//           display: "flex",
+//           border: "1px solid #ccc",
+//         }}
+//       >
+//         {inputs.map((input: any, index: number) => (
+//           <div key={index} style={{ marginRight: 10, flexShrink: 0 }}>
+//             <Input
+//               id="outlined-basic"
+//               key={Date.now()}
+//               value={input?.repetitions}
+//               sx={{ marginLeft: 3, width: 80 }}
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     );
+//   };
 
-  const table = useMaterialReactTable({
-    columns,
-    data,
-    enableColumnPinning: true,
-    layoutMode: "grid-no-grow",
-    initialState: {
-      columnPinning: { left: ["mrt-row-actions", "exercise.name"] },
-    },
-    enablePagination: false,
-    enableBottomToolbar: false,
-    enableFilters: false,
-  });
+  const [exercises, setExercices] = useState<Exercise[]>(data);
+
+  const addSet = (id: number) => {
+    const newExercise = exercises.map(
+      (exerciseMap: Exercise) => {
+        if (exerciseMap.exercise.id === id) {
+          exerciseMap.exercise.inputs = exerciseMap.exercise.inputs.concat(
+            <Input
+              id="outlined-basic"
+              key={Date.now()}
+              value={exerciseMap.exercise.sets[0].repetitions}
+              sx={{ marginLeft: 3, width: 80 }}
+            />
+          );
+        }
+        return exerciseMap;
+      }
+    );
+    setExercices(newExercise);
+  };
 
   return (
     <div style={{ marginTop: "1.5rem" }}>
-      <MaterialReactTable table={table} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          {exercises.map((exerciseMap: Exercise, index: number) => (
+            <div
+              key={index}
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingTop: "1rem",
+              }}
+            >
+              <div
+                style={{
+                  border: "1px solid",
+                  width: 140,
+                  height: 33,
+                  alignContent: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <span style={{ marginLeft: 15 }}>
+                  {exerciseMap.exercise.name}
+                </span>
+              </div>
+              <Button
+                key={index}
+                onClick={() => addSet(exerciseMap.exercise.id)}
+              >
+                <AddIcon />
+              </Button>
+              <div
+                key={Date.now()}
+                style={{
+                  width: 200,
+                  overflowX: "scroll",
+                  display: "flex",
+                  border: "1px solid #ccc",
+                }}
+              >
+                {exerciseMap.exercise.inputs.map((input, index) => (
+                  <div key={index}>
+                    <Input
+                      id="outlined-basic"
+                      key={Date.now()}
+                      value={input?.props.value}
+                      sx={{ marginLeft: 3, width: 80 }}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
